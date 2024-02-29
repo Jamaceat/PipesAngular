5e6570ab30aba452b56eb92900c914c1887df91c
Johan <johanoriginalplus@gmail.com>
2024-02-29 15:30:04 -0500

    Agregando i18nSelect

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class UncommonPageComponent {
+  // i18nSelect
+  public name: string = 'Johan';
 
+  public gender: 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    this.name = 'Selene';
+    this.gender = 'female';
+  }
 }
