6240bc0a7101bc21e1d73bd275ad5c18e39ef7ea
Johan <johanoriginalplus@gmail.com>
2024-02-29 11:14:07 -0500

    Agregando componentes de primeng

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``,
+})
+export class MenuComponent implements OnInit {
+  public items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Pipes de angular',
+        icon: 'pi pi-fw pi-download',
+        items: [
+          { label: 'Numeros', icon: 'pi pi-dollar' },
+          { label: 'No comunes', icon: 'pi pi-globe' },
+        ],
+      },
+    ];
+  }
+}
