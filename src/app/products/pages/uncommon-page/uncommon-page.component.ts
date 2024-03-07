647568aff2ed9f984fb6ae9228f40538bd2cebd2
Johan <johanoriginalplus@gmail.com>
2024-03-07 10:50:31 -0500

    async pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -45,4 +46,45 @@ export class UncommonPageComponent {
   deleteClient() {
     this.clients.shift();
   }
+
+  // KeyValue Pipe
+
+  public person = {
+    name: 'Johan',
+    age: 35,
+    address: 'Bogotá, Colombia',
+  };
+
+  public address = {
+    street: 'Calle 123',
+    city: 'Bogotá',
+    country: 'Colombia',
+  };
+
+  public heroes = [
+    {
+      name: 'Superman',
+      fly: true,
+    },
+    {
+      name: 'Batman',
+      fly: false,
+    },
+    {
+      name: 'Aquaman',
+      fly: false, // Aquaman can't fly
+    },
+  ];
+
+  // Async Pipe
+
+  public myObservableTimer = interval(2000);
+
+  public promiseValue: Promise<String> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
+
+  // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10...
 }
